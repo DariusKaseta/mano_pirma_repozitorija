b7274b0082e8d5e6452f118b0e4e723a04204300
DariusKaseta <kasetadarius@gmail.com>
2023-04-24 11:45:28 +0300

    main

---
+++ geometrija/bendras.py
@@ -0,0 +1,7 @@
+import math
+
+from geometrija.apskritimas import apskritimo_plotas as a_plotas
+from geometrija.kvadratas import kvadrato_plotas as k_plotas
+
+print(a_plotas(10))
+print(k_plotas(22))
---
+++ matematika.py
@@ -0,0 +1,12 @@
+def daugyba(x, y):
+    return x * y
+
+def dalyba(x, y):
+     if y != 0:
+        return x / y
+     else:
+        return "Klaida: dalyba iš nulio negalima"
+     
+
+
+
